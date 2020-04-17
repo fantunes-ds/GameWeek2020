4b46e08f59106770642fb3cd26b0eb536968fe7f
Lilian Gadeau <l.gadeau@student.isartdigital.com>
2020-04-17 10:23:25 -0400

    opening and closing animation for the door

---
+++ GameWeek 2020/Assets/Scripts/Script_Lilian/DoorBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    public Animator myAnimator = null;
+
+    private Transform colliderWorldTransform = null;
+    
+    private void Start()
+    {
+        colliderWorldTransform = GetComponent<BoxCollider>().transform;
+    }
+
+    private void Update()
+    {
+        GetComponent<BoxCollider>().transform.position = colliderWorldTransform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!myAnimator)
+            return;
+            
+        if (other.CompareTag("Player"))
+            myAnimator.SetTrigger("PlayerClose");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!myAnimator)
+            return;
+        
+        if (other.CompareTag("Player"))
+            myAnimator.SetTrigger("PlayerClose");
+    }
+}
